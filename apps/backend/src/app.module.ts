ada391ada52145352b91acac59ab6c6f89760988
Diego Trejo <124468131+trejo903@users.noreply.github.com>
2025-06-06 09:32:54 -0600

    Login-Completed

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoginModule } from './login/login.module';
+import { CreateAccountModule } from './create-account/create-account.module';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({
+      isGlobal:true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:typeOrmConfig,
+      inject:[ConfigService]
+    }),
+    LoginModule,
+    CreateAccountModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

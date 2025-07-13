6c731db9d950849f7e4997f9eba7e5b7b4fb7d6a
Diego Trejo <124468131+trejo903@users.noreply.github.com>
2025-07-13 15:15:33 -0600

    Update app.module.ts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LoginModule } from './login/login.module';
 import { CreateAccountModule } from './create-account/create-account.module';
 import { RoutesModule } from './routes/routes.module';
 import { ProfileModule } from './profile/profile.module';
@@ -44,7 +43,6 @@ import { Ubicaciones } from './ubicaciones/oficinas.module';
       useFactory:typeOrmConfig,
       inject:[ConfigService]
     }),
-    LoginModule,
     CreateAccountModule,
     RoutesModule,
     ProfileModule,

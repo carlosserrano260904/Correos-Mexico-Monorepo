4f3b5d17129581b14eddb35cf7f4a520096ce9cb
Kitaro <119483957+Andr000meda@users.noreply.github.com>
2025-07-26 21:16:46 -0600

    feat(guias): agregado numero de rastreo - El numero de rastreo ya no es un placeholder, pero solo el encabezado para envios nacionales.

--- apps/backend/src/guias_trazabilidad/business-logic/value-objects/numeroRastreo.vo.ts
+++ apps/backend/src/guias_trazabilidad/business-logic/value-objects/numeroRastreo.vo.ts
@@ -1,29 +1,37 @@
 import { Result } from '../../../utils/result';
 
 export class NumeroDeRastreoVO {
-  private constructor(private readonly numeroRastreo: string) { }
 
-  public static create(): Result<NumeroDeRastreoVO> {
-    const value = crypto.randomUUID() // TODO: eliminar este HARDCODE (simula la generacion del numero de rastreo)
-    if (/*value.trim().length != 13*/false) {
-      return Result.failure(
-        `El numero de rastreo ${value} debe ser de 13 caracteres`,
-      );
+  private constructor(
+    private readonly numeroRastreo: string
+  ) { }
+
+  public static safeCreate(): NumeroDeRastreoVO {
+
+    const servicios = { 'Estandar': 'CA', 'Express': 'EE', 'Menor': 'RT' }
+
+    const pais = 'MX';
+
+    function genId(): number {
+      const min = 100_000_000;
+      const max = 999_999_999;
+      return Math.floor(Math.random() * (max - min + 1) + min);
     }
-    return Result.success(new NumeroDeRastreoVO(value));
+
+    const id = genId().toString()
+
+    const value = `${servicios.Express}${pais}${id}`
+
+    return new NumeroDeRastreoVO(value);
   }
 
   public static fromString(value: string): Result<NumeroDeRastreoVO> {
-    if (/*value.trim().length != 13*/false) {
+    if (value.trim().length != 13) {
       return Result.failure(
         `El numero de rastreo ${value} debe ser de 13 caracteres`,
       );
     }
     return Result.success(new NumeroDeRastreoVO(value))
-  }
-
-  public static safeCreate(): NumeroDeRastreoVO {
-    return new NumeroDeRastreoVO(crypto.randomUUID());
   }
 
   public static fromPersistence(value: string): NumeroDeRastreoVO {

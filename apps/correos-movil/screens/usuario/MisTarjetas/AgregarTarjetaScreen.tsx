239dd61f011d3530c6caa5ca5e5ad1c6087bb199
rosita2423 <victorhugro@hotmail.com>
2025-10-14 19:49:49 -0600

    Corrección Mis Tarjetas Redirección
    Corrección de error en el cual se redireccionaba a una vista erronea cuando se añadia una tarjeta.

--- apps/correos-movil/screens/usuario/MisTarjetas/AgregarTarjetaScreen.tsx
+++ apps/correos-movil/screens/usuario/MisTarjetas/AgregarTarjetaScreen.tsx
@@ -57,8 +57,12 @@ export default function AgregarTarjetaScreen() {
         paymentMethodId: paymentMethod.id,
         profileId: profileId, // 👈 este dato es crucial
       });
-  setIsSaving(false);
-  navigation.goBack();
+    setIsSaving(false);
+
+    // Redirección a la pantalla MisTarjetasScreen.
+    navigation.navigate('MisTarjetasScreen');
+    Alert.alert('Tarjeta añadida correctamente');
+
     } catch (err: any) {
       const backendMsg = err?.response?.data?.message || err.message || 'No se pudo guardar la tarjeta.';
       console.error('Error al agregar tarjeta:', backendMsg);

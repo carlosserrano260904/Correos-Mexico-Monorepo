b5a53b9feeb8dea754ee4dc3364f6de0d60ba1ad
Carlos Angel Serrano Rangel <carlosgaming260904@gmail.com>
2025-10-25 17:19:34 -0600

    fix(optimizar-modulo-repartidor): Cambios leves de MainLoadPackagesDistributor.tsx
    Se hizo el cambio para hacer el rebase

--- apps/correos-movil/screens/repartidor/MainLoadPackagesDistributor.tsx
+++ apps/correos-movil/screens/repartidor/MainLoadPackagesDistributor.tsx
@@ -12,7 +12,7 @@ import * as Location from 'expo-location';
 import { LatLng } from "react-native-maps";
 import { useFocusEffect, useRoute } from "@react-navigation/native";
 import axios from "axios";
-import PackageCard from "../../components/DistributorComponents/PackageCard";   
+import PackageCard from "../../components/DistributorComponents/PackageCard";
 
 const screenWidth = Dimensions.get('screen').width
 const screenHeight = Dimensions.get('screen').height
@@ -485,7 +485,7 @@ export default function MainLoadPackagesDistributor({navigation}: any) {
             optimizedIntermediates={optimizedIntermediates} 
             routePoints={routePoints}
             packages={packages}/>,
-
+        
         second: () => <PackageCard packages={packages} optimizedIntermediates={optimizedIntermediates} />,
     });
     

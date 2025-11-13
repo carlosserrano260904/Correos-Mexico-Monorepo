d2864565e6d8501955b9b5683375b23157ac7600
Emmanuel <bayonaj368@gmail.com>
2025-11-13 10:33:56 -0600

    chore: change confi to resolve path mapping on vercel

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,4 @@
+import 'module-alias/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';

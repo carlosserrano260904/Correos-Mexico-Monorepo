1cae59db7c975f9af0a74336f01cc684f1e43944
Imri <imri.adame.is@unipolidgo.edu.mx>
2025-11-23 22:27:58 -0600

    fixt-frontend: Login
    cambio menor al texto

--- apps/frontend_web/src/app/login/page.tsx
+++ apps/frontend_web/src/app/login/page.tsx
@@ -185,7 +185,7 @@ const Login = () => {
           </div>
 
           <p className="text-center text-sm text-gray-600">
-            No ¿Ya tienes una cuenta?{" "}
+            ¿No tienes cuenta?{" "}
             <Link href="/registro" className="text-pink-600 hover:underline">
               Ingresar
             </Link>

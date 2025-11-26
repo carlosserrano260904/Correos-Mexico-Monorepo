49555afb77571053d8096d9f7f7e9fc1e8593cb3
Emmanuel <bayonaj368@gmail.com>
2025-11-26 13:52:05 -0600

    1.0.2

--- apps/correos-movil/app.config.js
+++ apps/correos-movil/app.config.js
@@ -5,7 +5,7 @@ module.exports = ({ config }) => ({
   expo: {
     name: "Correos de Mexico",
     slug: "correos-de-mexico",
-    version: "1.0.1",
+    version: "1.0.2",
     scheme: "correosdemexico",
     assetBundlePatterns: ["**/*"],
     orientation: "portrait",

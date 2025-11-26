9d5253a9c4d0abc384b8155ef860a977ebe82e31
artic000 <faridy.rr.26@gmail.com>
2025-11-26 15:35:23 -0600

    fix: resolve Descuento component import error

--- apps/frontend_web/src/app/Vendedor/app/Descuentos/Componentes/tableroDescuentos.tsx
+++ apps/frontend_web/src/app/Vendedor/app/Descuentos/Componentes/tableroDescuentos.tsx
@@ -7,7 +7,7 @@ import {
 } from "../../../../../components/ui/table"
 import { DescuentoProps } from "../../../../../types/interface"
 import { Separator } from "../../../../../components/ui/separator"
-import { Descuento } from "../../../components/primitivos"
+import { Descuento } from '@/components/primitivos';
 
 interface TablerDescuentosProps {
   entradas: DescuentoProps[]

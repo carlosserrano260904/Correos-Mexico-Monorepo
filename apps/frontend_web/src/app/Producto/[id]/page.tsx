dd581030eb2658445bd70e7bd7cb50af41eba926
artic000 <faridy.rr.26@gmail.com>
2025-11-30 11:29:17 -0600

    Refactor: Import CarouselDetalles and ReviewsSection in ProductDetailPage

--- apps/frontend_web/src/app/Producto/[id]/page.tsx
+++ apps/frontend_web/src/app/Producto/[id]/page.tsx
@@ -7,7 +7,8 @@ import { ProductDetails } from '@/components/primitivos/ProductDetails';
 import { ProductStory } from '@/components/primitivos/ProductStory';
 import { Plantilla } from '@/components/plantilla';
 import { CarrouselProducts } from '@/components/CarouselProducts';
-import { ReviewsSection } from '@/components/ReviewsSection'; // Importar el ReviewsSection completo
+import { ReviewsSection } from '@/components/ReviewsSection'; 
+import { CarouselDetalles } from '@/components/CarouselDetalles';
 
 export default function ProductDetailPage() {
   const params = useParams();

7db66bf857c6b160553630458a4ab18f1064add6
Imri <imri.adame.is@unipolidgo.edu.mx>
2025-11-30 19:31:46 -0600

    fix-frontend: redireccion de navbar
    -Corrección a la ruta de historial de compras de la navbar

--- apps/frontend_web/src/components/navbar.tsx
+++ apps/frontend_web/src/components/navbar.tsx
@@ -496,7 +496,7 @@ export const Navbar = () => {
                                         Configuracion
                                     </Link>
                                     
-                                    <Link href="/historial" onClick={handleDropdownClose} className="text-gray-700 hover:text-gray-900 font-medium text-sm sm:text-base flex items-center gap-2 transition-colors">
+                                    <Link href="/historial-de-compras" onClick={handleDropdownClose} className="text-gray-700 hover:text-gray-900 font-medium text-sm sm:text-base flex items-center gap-2 transition-colors">
                                         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
                                         </svg>
